6e289effc536be486c979eb70ba9e0fc7c19c426
Kelly J Andrews <kelly@kellyjandrews.com>
2024-12-28 21:15:33 -0500

    replace all with web only

--- apps/native/app/_layout.tsx
+++
@@ -1,7 +0,0 @@
-import { Stack } from "expo-router";
-import "../global.css";
-import React from "react";
-
-export default function RootLayout() {
-  return <Stack />;
-}
---
+++ web/app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+ 
+import Hero from "../components/hero";
+import List from "../components/productList";
+
+export default function Home() {
+  return (
+    <div>
+    <Hero />
+    <List />
+    <List />
+    <List />
+    </div>
+
+  );
+}
